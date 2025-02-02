1bb973e6b6cf5ac771e6bf2aeb0852efa71bc70d
amitshethe <ashethe@deqode.com>
2025-02-02 17:25:28 +0530

    Category Crud operation completed

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -22,5 +22,60 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display order cannot be same");
+            }
+            if (ModelState.IsValid) 
+            {
+                var res = _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            { 
+                return NotFound(); 
+            } 
+            Category? categoryFind = _db.categories.Find(id);
+            return View(categoryFind);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name and Display order cannot be same");
+            }
+            if (ModelState.IsValid)
+            {
+                var res = _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFind = _db.categories.Find(id);
+            if (categoryFind == null)
+            { return NotFound(); }
+            var res = _db.categories.Remove(categoryFind);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
     }
 }
